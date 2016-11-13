07b89a9c8c9bc4e915c7aca0f388a9510a28b7f5
Daniel Hug <daniel@hugwebdesign.com>
2016-11-13 12:35:33 -0800

    swap apps

--- js/main.js
+++ js/main.js
@@ -1,86 +1,31 @@
+/* DOM helpers
+**************************************/
 
-function createCode(){
-var text = document.getElementById("lyrics").value;
-var root, substring, letter, j;
-var usedletters = [];
-var unusedletters = [];
-var compressedletters = [];
-
-// generate an array of all possible letters:
-for (var i = 32; i <= 126; i++) {
-  unusedletters.push(String.fromCharCode(i));
+// Get element by CSS selector
+function qs(selector, scope) {
+	return (scope || document).querySelector(selector);
 }
 
-// find unused letters, they can be used for replacements later on
-text.split("").map(function(value) {
-  if (usedletters.indexOf(value) == -1) {
-    usedletters.push(value);
-  }
-  j = unusedletters.indexOf(value);
-  if (j != -1) {
-    unusedletters.splice(j, 1);
-  }
+// Add event listeners
+function on(target, type, callback, useCapture) {
+	target.addEventListener(type, callback, !!useCapture);
+}
+
+
+
+// get elements
+var btn = qs('#go');
+var input = qs('#input');
+var output = qs('#output');
+
+// handle button click
+on(btn, 'click', function() {
+	// Create a new SuffixTree from the input text
+	var root = new SuffixTree(input.value);
+
+	// Get the longest repeated substring
+	var substring = root.node.getLongestRepeatedSubString();
+
+	// place the substring in the output
+	output.value = substring;
 });
-
-// make it html friendly:
-unusedletters.splice(unusedletters.indexOf("\""),1);
-unusedletters.splice(unusedletters.indexOf("'"),1);
-unusedletters.splice(unusedletters.indexOf("`"),1);
-unusedletters.splice(unusedletters.indexOf("'"),1);
-unusedletters.splice(unusedletters.indexOf("<"),1);
-unusedletters.splice(unusedletters.indexOf(">"),1);
-unusedletters.splice(unusedletters.indexOf("/"),1);
-unusedletters.splice(unusedletters.indexOf("\\"),1);
-
-// turn Newlines into html <br>, this changes the lyrics, 
-// but it's nice in the end to view it in a browser
-letter = unusedletters.pop();
-text = text.replace(/\n/g, letter);
-text = text + letter + "<br>";
-compressedletters.push(letter);
-
-while (unusedletters.length) {
-  // Crete a new SuffixTree from the current text
-  root = new SuffixTree(text);
-  // Get the longest repeated substring
-  substring = root.node.getLongestRepeatedSubString();
-  // If the substring is too short, nothing is gained by substitution
-  if (substring.length == 4) break;
-  // get a new letter for substitution
-  letter = unusedletters.pop();
-  // replace all occurences of the substring with the letter
-  while (text.indexOf(substring) != -1) {
-    text = text.replace(substring, letter);
-  }
-  // append the letter and the substring for decompression
-  text = text + letter + substring;
-  // remember the letter
-  compressedletters.push(letter);
-}
-
-compressedletters = compressedletters.join("");
-
-/////////////////////////////////////////////
-// Create code
-/////////////////////////////////////////////
-code = 
-  't="' + text + '";' +
-  'c="' + compressedletters + '".split("");' +
-  "while(l=c.pop()){" +
-    "t=t.split(l);" + 
-    "t=t.join(t.pop())" + 
-  "}" + 
-  "document.write(t)";
-  // + "alert(t)";
-
-document.getElementById("code").innerHTML = code.replace(/>/g, "&gt;").replace(/</g, "&lt;");
-document.getElementById("length").innerHTML = code.length;
-}
-
-document.getElementById("eval").onclick = function(){
-  document.getElementById("uncompressed").innerHTML = eval(code);
-}
-
-createCode();
-
-document.getElementById("newcode").onclick = createCode;
