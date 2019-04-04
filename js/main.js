803f3a90b8523400a86e09cd81e465ec37f17b84
Daniel Hug <daniel@hugwebdesign.com>
2019-04-03 21:36:31 -0700

    Isolate 3-

--- js/main.js
+++ js/main.js
@@ -24,11 +24,5 @@ function log(msg) {
 
 // handle button click
 on(btn, 'click', function() {
-    // get the input text
-    var in = input.value;
-
-    // go through each day
-    var days = in.split('\n\n');
-    
 });
 log('test')
