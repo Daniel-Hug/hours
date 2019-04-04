d8470f69b04e8b6a8c87a273eceaf365aec8ff3b
Daniel Hug <daniel@hugwebdesign.com>
2019-04-03 22:36:53 -0700

    Rename in var - reserved word

--- js/main.js
+++ js/main.js
@@ -21,7 +21,40 @@ function log(msg) {
 }
 
 // handle button click
-on(btn, 'click', function() {
-    log(input.value);
+// handle button click
+on(btn, 'click', function() {try{
+    // get the input text
+    var inVal = input.value;
+
+    // go through each day
+    var days = inVal.split('\n\n');
+    days = days.map(function(daySrc) {
+        var dayParts = daySrc.split('\n');
+        var startT = dayParts[1];
+        var endT = dayParts[2];
+        var startTParts = startT.split(':');
+        var endTParts = endT.split(':');
+        var startHour = parseInt(startTParts[0], 10) % 12;
+        var endHour = parseInt(endTParts[0], 10) % 12;
+        var startMin = parseInt(startTParts[1], 10);
+        var endMin = parseInt(endTParts[1], 10);
+        var finHour = 0;
+        var finMin = 0;
+        for(var minCounter=startMin; minCounter !== endMin; minCounter = (minCounter + 1) % 60) {
+            finMin++;
+        }
+        for(var hourCounter=startHour; hourCounter !== endHour; hourCounter = (hourCounter + 1) % 12) {
+            finHour++;
+        }
+        var fin = finHour + finMin / 60;
+        return daySrc + fin;
+    });
+	var finSrc = days.join('');
+
+	// give the output
+	output.value += finSrc;
+    } catch(err) {
+        output.value += err;
+    }
 });
 log('test')
