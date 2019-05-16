6633f25748a78928b0247a33f1fec18b5490d208
Daniel Hug <daniel@hugwebdesign.com>
2019-05-16 12:35:49 -0700

    Handle any length break

--- js/main.js
+++ js/main.js
@@ -11,6 +11,15 @@ function on(target, type, callback, useCapture) {
 	target.addEventListener(type, callback, !!useCapture);
 }
 
+function parseDuration(src) {
+  var isHour = src.toUpperCase().indexOf('HOUR') >= 0;
+  var isMin = src.toUpperCase().indexOf('MIN') >= 0;
+  var num = parseInt(src, 10);
+  if (isNan(num)) num=1;
+  if (isHour) return num;
+  if (isMin) return num/60;
+}
+
 // get elements
 var btn = qs('#go');
 var input = qs('#input');
@@ -29,10 +38,13 @@ on(btn, 'click', function() {try{
     var days = inVal.split('\n\n');
     days = days.map(function(daySrc) {
         var isLunch = daySrc.toUpperCase().indexOf('NO LUNCH') < 0;
-        var breakHrs = isLunch ? 0.5 : 0;
+        var isLunchSpecified = isLunch &&  daySrc.toUpperCase().indexOf('LUNCH') >= 0;
         var dayParts = daySrc.split('\n');
         var startT = dayParts[1];
         var endT = dayParts[2];
+        var lunchSrc = dayParts[dayParts.length - 1];
+        var breakHrs = isLunch ? 0.5 : 0;
+        if (isLunchSpecified) breakHrs = parseDuration(lunchSrc);
         var startTParts = startT.split(':');
         var endTParts = endT.split(':');
         var startHour = parseInt(startTParts[0], 10) % 12;
